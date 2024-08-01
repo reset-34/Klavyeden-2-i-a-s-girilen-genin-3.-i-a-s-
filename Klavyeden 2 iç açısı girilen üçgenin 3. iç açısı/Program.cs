9f8961d8c9fa7ab3da40ffbd7220f66a8a1a1cb6
Serap BALCI <81075168+reset-34@users.noreply.github.com>
2024-08-01 15:52:28 +0300

    Add project files.

---
+++ Klavyeden 2 iç açısı girilen üçgenin 3. iç açısı/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klavyeden_2_iç_açısı_girilen_üçgenin_3.iç_açısı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. iç açıyı girin");
+            int aci1 = Int32.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("2. iç açıyı girin");
+            int aci2 = Int32.Parse(Console.ReadLine());
+
+            int toplam = aci1 + aci2;
+            int bilinmeyenaci = 180 - toplam;
+
+            Console.WriteLine("Bilinmeyen Üçüncü açı=" + bilinmeyenaci);
+
+            Console.ReadLine();
+        }
+    }
+}
